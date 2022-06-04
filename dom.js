ae084f45337d07ab74b63b811a5458c21bea82bc
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-05 02:41:24 +0530

    tagname

--- dom.js
+++ dom.js
@@ -12,20 +12,27 @@
 // addItems.style.color = "green";
 // console.log(addItems);
 
-var items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent = "Hello 2";
-items[1].style.fontWeight = "bold";
-items[1].style.backgroundColor = "yellow";
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello 2";
+// items[1].style.fontWeight = "bold";
+// items[1].style.backgroundColor = "yellow";
 
-for (var i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "#f4f4f4";
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "#f4f4f4";
+// }
+
+// //third element in list - green bg
+// items[2].style.backgroundColor = "green";
+
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+
+var li = document.getElementsByTagName("li");
+for (var i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = "#f4f4f4";
 }
 
-//third element in list - green bg
-items[2].style.backgroundColor = "green";
-
-for (var i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
-}
+li[4].classList.add("list-group-item");
