5fecfebbdfb14fd18093af927acc15e928d116b0
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-05 02:11:30 +0530

    second

--- dom.js
+++ dom.js
@@ -2,10 +2,11 @@
 // console.log(document.URL);
 // console.log(document.activeElement);
 
-var headerTitle = document.getElementById("header-title");
-var header = document.getElementById("main-header");
-console.log(headerTitle);
-header.style.borderBottom = "solid 3px #000";
+//console.log(document.getElementById('header-title));
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// // console.log(headerTitle);
+// header.style.borderBottom = "solid 3px #000";
 
 var addItems = document.querySelector(".title");
 addItems.innerHTML = "<B>Add Items</B>";
