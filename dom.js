2b5ecaf9bba20ed793ce3cdc2b58c8c7a993422c
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-05 02:23:33 +0530

    getelementByclassname

--- dom.js
+++ dom.js
@@ -2,12 +2,30 @@
 // console.log(document.URL);
 // console.log(document.activeElement);
 
-var headerTitle = document.getElementById("header-title");
-var header = document.getElementById("main-header");
-console.log(headerTitle);
-header.style.borderBottom = "solid 3px #000";
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// console.log(headerTitle);
+// header.style.borderBottom = "solid 3px #000";
 
-var addItems = document.querySelector(".title");
-addItems.innerHTML = "<B>Add Items</B>";
-addItems.style.color = "green";
+// var addItems = document.querySelector(".title");
+// addItems.innerHTML = "<B>Add Items</B>";
+// addItems.style.color = "green";
 // console.log(addItems);
+
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[1]);
+items[1].textContent = "Hello 2";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = "#f4f4f4";
+}
+
+//third element in list - green bg
+items[2].style.backgroundColor = "green";
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
