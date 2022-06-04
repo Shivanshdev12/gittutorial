234b8194f5e6affa4303f28d09d434f960aad58d
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-05 02:15:45 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.activeElement);
+
+var headerTitle = document.getElementById("header-title");
+var header = document.getElementById("main-header");
+console.log(headerTitle);
+header.style.borderBottom = "solid 3px #000";
+
+var addItems = document.querySelector(".title");
+addItems.innerHTML = "<B>Add Items</B>";
+addItems.style.color = "green";
+// console.log(addItems);
