4ad7de79a06d4abda98dbdb0ccab88f6cb247ba8
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-05 01:53:11 +0530

    third commit

--- dom.js
+++ dom.js
@@ -2,11 +2,10 @@
 // console.log(document.URL);
 // console.log(document.activeElement);
 
-//console.log(document.getElementById('header-title));
-// var headerTitle = document.getElementById("header-title");
-// var header = document.getElementById("main-header");
-// // console.log(headerTitle);
-// header.style.borderBottom = "solid 3px #000";
+var headerTitle = document.getElementById("header-title");
+var header = document.getElementById("main-header");
+console.log(headerTitle);
+header.style.borderBottom = "solid 3px #000";
 
 var addItems = document.querySelector(".title");
 addItems.innerHTML = "<B>Add Items</B>";
