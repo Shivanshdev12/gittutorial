c7c4b502d9c1be4d77b27ee0cabd0ec403ba8c63
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-19 16:13:35 +0530

    setInterval

--- async/script.js
+++ async/script.js
@@ -1,13 +1,25 @@
 let posts = [
-  { title: "Post One", body: "This is post one" },
-  { title: "Post Two", body: "This is post two" },
+  {
+    title: "Post One",
+    body: "This is post one",
+    createdAt: new Date().getTime(),
+  },
+  {
+    title: "Post Two",
+    body: "This is post two",
+    createdAt: new Date().getTime(),
+  },
 ];
 
+let timerId = 0;
 function getPosts() {
-  setTimeout(() => {
+  clearInterval(timerId);
+  timerId = setInterval(() => {
     let output = "";
     posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
+      output += `<li>${post.title} - last updated ${Math.round(
+        (new Date().getTime() - post.createdAt) / 1000
+      )} seconds ago</li>`;
     });
     document.body.innerHTML = output;
   }, 1000);
@@ -15,7 +27,10 @@ function getPosts() {
 
 function createPost(post, callback) {
   setTimeout(() => {
-    posts.push(post);
+    posts.push({
+      ...post,
+      createdAt: new Date().getTime(),
+    });
     callback();
   }, 1000);
 }
@@ -26,6 +41,18 @@ function create4thPost(post, callback) {
   }, 1000);
 }
 
-create4thPost({ title: "Post Four", body: "This is post four" }, createPost);
+create4thPost(
+  {
+    title: "Post Four",
+    body: "This is post four",
+  },
+  createPost
+);
 
-createPost({ title: "Post Three", body: "This is post three" }, getPosts);
+createPost(
+  {
+    title: "Post Three",
+    body: "This is post three",
+  },
+  getPosts
+);
